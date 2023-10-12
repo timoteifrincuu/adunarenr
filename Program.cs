858a674829bed423de92f0b331111b8858c968f9
timoteifrincuu <146390788+timoteifrincuu@users.noreply.github.com>
2023-10-12 22:10:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace adunareanr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, rez;
+
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+
+            rez = x + y;
+
+            Console.WriteLine(rez);
+        }
+        
+    }
+}
